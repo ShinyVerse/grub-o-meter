54c9c127867d0945e23ffe2b8e7dd17a4aa3c7af
Laura Jackson <playeronereadystart@gmail.com>
2019-01-30 13:33:36 +0000

    manual tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import MealList from './components/MealList/MealList';
+import ImageTallyItem from './components/ImageTallyItem/ImageTallyItem';
+import iconWater from './assets/water.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    const iconClicker = (id) => {
+      console.log(id);
+    }
     const list = [
        {
           title:'Breakfast',
@@ -15,16 +21,24 @@ class App extends Component {
         },
         {
           title:'Dinner',
-          id: 'meal2'
+          id: 'meal3'
         },
         {
           title:'Snacks',
-          id: 'meal2'
+          id: 'meal4'
         },
       ]
     return (
       <div data-test='component-main-div' className='App'>
         <MealList list={list} />
+        <ImageTallyItem icon={iconWater}
+          isSelected={true}
+          id='icon-water-1'
+          iconClicker={iconClicker} />
+          <ImageTallyItem icon={iconWater}
+            isSelected={false}
+            id='icon-water-2'
+            iconClicker={iconClicker} />
       </div>
     );
   }
